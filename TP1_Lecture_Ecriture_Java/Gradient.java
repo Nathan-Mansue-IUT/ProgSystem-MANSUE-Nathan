ef680504e945a9a6189941d4ab1444bae5e89739
NATHAN MANSUE <nathan.mansue@iut.local>
2026-09-11 16:10:04 +0200

    Classe Gradient fonctionnel

--- TP1_Lecture_Ecriture_Java/Gradient.java
+++ TP1_Lecture_Ecriture_Java/Gradient.java
@@ -1,17 +1,18 @@
 public class Gradient {
     public static void main(String[] args) {
+		
         Image img = new Image(200, 100);
 
         // Génération du dégradé de bleu
-        for (int y = 0; y < hauteur; y++) {
-            for (int x = 0; x < largeur; x++) {
-                int bleu = 0; // quel calcule ?
+        for (int y = 0; y < img.getHeight(); y++) {
+            for (int x = 0; x < img.getWidth(); x++) {
+                int bleu = x; // quel calcule ?
                 img.setPixel(x, y, 0, 0, bleu);
             }
         }
 
         try {
-            img.save("gradient.ppm");
+            img.save_txt("gradient.ppm");
             System.out.println("Dégradé créé avec succès !");
         } catch (Exception e) {
             System.err.println("Erreur lors de la création du dégradé : " + e.getMessage());
--- TP1_Lecture_Ecriture_Java/Image.java
+++ TP1_Lecture_Ecriture_Java/Image.java
@@ -12,17 +12,17 @@ public class Image {
     /**
      * Constructeur : initialise une image vide.
      */
-    public Image(int width, int hauteur) {
+    public Image(int width, int height) {
         this.width = width;
         this.height = height;
-        pixels = new int[hauteur][largeur][3];
+        pixels = new int[height][width][3];
     }
 
     /**
      * Définit la couleur d'un pixel à la position (x, y)
      */
     public void setPixel(int x, int y, int r, int g, int b) {
-        if (x >= 0 && x < largeur && y >= 0 && y < hauteur) {
+        if (x >= 0 && x < width && y >= 0 && y < height) {
             pixels[y][x][0] = r;
             pixels[y][x][1] = g;
             pixels[y][x][2] = b;
@@ -34,7 +34,7 @@ public class Image {
      */
     public void save_txt(String filename) throws IOException {
         try {
-            FileWriter writer = new FileWriter("FirstPPM.ppm");
+            FileWriter writer = new FileWriter(filename);
 
             writer.write("P3\n");
             // Écriture des dimensions
@@ -43,9 +43,9 @@ public class Image {
 			writer.write("255\n");
             // Écriture des pixels
 			
-			for(int i = 0 ; i <= getWidth() ; i++) {
+			for(int i = 0 ; i < getHeight() ; i++) {
 				
-				for(int j = 0 ; j <= getHeight() ; j++) {
+				for(int j = 0 ; j < getWidth() ; j++) {
 					writer.write(pixels[i][j][0] + " " + pixels[i][j][1] + " " + pixels[i][j][2] + " ");
 				}
 				writer.write("\n");
